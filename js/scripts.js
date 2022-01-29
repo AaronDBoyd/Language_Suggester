8c671d6dffe8e0716e130031a33a83c4c3665d9c
Aaron Boyd <a.boyd91@yahoo.com>
2022-01-29 11:05:20 -0800

    update if else functions so that the other results hide when forms are resubmitted

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,26 @@ $(document).ready(function() {
 
     if (hundred <= 30 && vacation === "home" || music === "xsj") {
       $("#ruby").show();
+      $("#go").hide();
+      $("#python").hide();
+      $("#nope").hide();
     } else if (hundred > 30 && vacation === "beach" ) {
       $("#go").show();
+      $("#ruby").hide();
+      $("#python").hide();
+      $("#nope").hide();
     } else {
       $("#python").show();
+      $("#ruby").hide();
+      $("#go").hide();
+      $("#nope").hide();
     }
     
     if (dogs === "false") {
       $("#nope").show();
+      $("#ruby").hide();
+      $("#python").hide();
+      $("#go").hide();
     }
     
     event.preventDefault();
